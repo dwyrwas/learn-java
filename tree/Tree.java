e759cae7ee6f23fcd59e3ba617ac248196ea29ee
Duncan Wyrwas <32854279+dwyrwas@users.noreply.github.com>
2022-07-09 22:06:57 -0500

    Create Tree.java

---
+++ tree/Tree.java
@@ -0,0 +1,13 @@
+// Define your tree class in this file!
+public class Tree {
+  public static void main(String[] args) {
+    //This function introduces ourselves and plants a tree
+    System.out.println("Hi there! My name is Duncan and I am learning to code in Java!");
+    System.out.println("I am going to plant a tree today!");
+    System.out.println("  *  ");
+    System.out.println(" *** ");
+    System.out.println(" *** ");
+    System.out.println("  *  ");
+    System.out.println("  *  ");  
+  }
+}
